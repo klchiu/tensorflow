d976e0791c4f854afe625f8c83cec3e1ba8fb2c8
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2023-03-23 14:20:01 -0400

    Add cfg files for add, sub, mult

---
+++ tensorflow/esp_libs/cfg_tf_add3.h
@@ -0,0 +1,74 @@
+// Copyright (c) 2011-2023 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+#ifndef __CFG_TF_ADD3_H__
+#define __CFG_TF_ADD3_H__
+
+#include "lib_esp.h"
+// #include "tf_add3_stratus.h"
+#include <linux/ioctl.h>  // for _IOW()
+
+extern "C" {
+struct tf_add3_stratus_access {
+	struct esp_access esp;
+
+	unsigned int tf_length;
+	unsigned int tf_src_dst_offset_0;	// output
+	unsigned int tf_src_dst_offset_1;	// input 1
+	unsigned int tf_src_dst_offset_2;	// input 2
+	unsigned int src_offset;
+	unsigned int dst_offset;
+};
+}
+
+#define TF_ADD3_STRATUS_IOC_ACCESS	_IOW ('S', 0, struct tf_add3_stratus_access)
+
+
+
+// typedef uint64_t token_t;
+typedef float native_t;
+
+#define fx2float             fixed64_to_double
+#define float2fx             double_to_fixed64
+#define FX_IL           34
+
+struct esp_access esp2_add3 =
+    {
+        .contig = NULL,
+        .run = 0,
+        .p2p_store = 0,
+        .p2p_nsrcs = 0,
+        .p2p_srcs = {"", "", "", ""},
+        .coherence = ACC_COH_NONE,
+        .footprint = 0,
+        .alloc_policy = CONTIG_ALLOC_PREFERRED,
+        .ddr_node = 0,
+        .in_place = 0,
+        .reuse_factor = 0,
+};
+
+struct tf_add3_stratus_access tf_add3_cfg_000[] = {
+	{
+        .esp = esp2_add3,
+		/* <<--descriptor-->> */
+		.tf_length = 1024,
+		.tf_src_dst_offset_0 = 0,
+		.tf_src_dst_offset_1 = 1024,
+		.tf_src_dst_offset_2 = 2048,
+		.src_offset = 0,
+		.dst_offset = 0,
+	}
+};
+
+esp_thread_info_t cfg_tf_add3[] = {
+	{
+		.run = true,
+		.devname = "tf_add3_stratus.0",
+        .hw_buf = NULL,
+        .ioctl_req = TF_ADD3_STRATUS_IOC_ACCESS,
+        .esp_desc = &(tf_add3_cfg_000[0].esp),
+        .fd = 0,
+        .hw_ns = 0,
+	}
+};
+
+#endif /* __CFG_TF_ADD3_H__ */
